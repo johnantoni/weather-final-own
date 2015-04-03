834c0e81c3f5b4fba327742efd51b48c6cc6b01f
R. Tony Goold <tony@wattpad.com>
2015-04-02 20:38:23 -0400

    Fetch dummy data from Dark Sky

--- Weather/ViewController.swift
+++ Weather/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 import CoreData
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, NSURLConnectionDataDelegate {
+
+    var successful = false
+    var buffer = NSMutableData()
+
 
     var context: NSManagedObjectContext!
     var model: NSManagedObjectModel!
@@ -24,7 +28,84 @@ class ViewController: UIViewController, UITableViewDataSource {
         request.sortDescriptors = [ NSSortDescriptor(key: "date", ascending: true) ]
         self.fetchController = NSFetchedResultsController(fetchRequest: request, managedObjectContext: self.context, sectionNameKeyPath: nil, cacheName: nil)
         self.fetchController.performFetch(nil)
+
+
+//        let jsonString = "{ \"longitude\": 123.45, \"latitude\": -34.56 }"
+//        if let data = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
+//            handleResponseData(data)
+//        }
+
+        self.sendRequest()
     }
+
+    func sendRequest() {
+        if let url = NSURL(string: "https://api.forecast.io/forecast/<api key>/43.65,-79.37") {
+            var request = NSMutableURLRequest(URL: url)
+            // request.HTTPMethod = "POST"
+            var connection = NSURLConnection(request: request, delegate: self)
+        }
+    }
+
+
+    func handleResponseData(data: NSData) {
+        var error: NSError? = nil
+        var json: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &error)
+        if let dictionary = json as? NSDictionary {
+            let latitude = dictionary["latitude"] as? Float
+            let longitude = dictionary["longitude"] as? Float
+            println("Lat: \(latitude), Long: \(longitude)")
+        }
+        else if error != nil {
+            println("Failed to decode JSON: \(error!)")
+        }
+    }
+
+    func connection(connection: NSURLConnection, didReceiveResponse response: NSURLResponse) {
+        if let httpResponse = response as? NSHTTPURLResponse {
+            let status = httpResponse.statusCode
+            println("didReceiveResponse: \(status) \(NSHTTPURLResponse.localizedStringForStatusCode(status))")
+            if status >= 200 && status < 400 {
+                self.successful = true
+            }
+        }
+        self.buffer.length = 0
+    }
+
+    func connection(connection: NSURLConnection, didReceiveData data: NSData) {
+        println("didReceiveData: \(data.length) bytes")
+        self.buffer.appendData(data)
+
+        // Update progress bar?
+    }
+
+    func connectionDidFinishLoading(connection: NSURLConnection) {
+        println("connectionDidFinishLoading")
+        // Do stuff with the response
+        self.handleResponseData(self.buffer)
+
+        // Be kind, free up memory
+        self.buffer.length = 0
+    }
+
+    func connection(connection: NSURLConnection, didFailWithError error: NSError) {
+        self.successful = false
+        println("Failed: \(error)")
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
